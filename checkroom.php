baab7895b5f5f4dc3b1557f7a2165412fb104520
Viral khatri <blackops150804@gmail.com>
2021-07-19 15:49:19 +0530

    check once

--- checkroom.php
+++ checkroom.php
@@ -39,7 +39,12 @@ $_SESSION['childrens'] = $_POST["totalchildrens"];
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
 <meta class="foundation-data-attribute-namespace"><meta class="foundation-mq-xxlarge"><meta class="foundation-mq-xlarge"><meta class="foundation-mq-large"><meta class="foundation-mq-medium"><meta class="foundation-mq-small"><style></style><meta class="foundation-mq-topbar"></head>
+
+
+<body>
 <body class="fontbody">
+
+
 <div class="row foo" style="margin:30px auto 30px auto;">
 <div class="large-12 columns">
 		<div class="large-3 columns centerdiv">
@@ -147,7 +152,7 @@ $_SESSION['childrens'] = $_POST["totalchildrens"];
 		</div>
 		<div class="large-12 columns" id="roomselected" style="display:none;" >
 				<hr>
-							<br><label for="submit-form" class="book button small fontslabo" style="background-color:#2ecc71; width:100%; height:45px; !important;">Proceed To Book</label><!--button name="submit" href="#" class="button small fontslabo" style="background-color:#2ecc71; width:100%;" >Proceed Booking</button-->
+							<br><label for="submit-form" class="book button small fontslabo" style="background-color:#2ecc71; width:100%; height:45px;">Proceed To Book</label><!--button name="submit" href="#" class="button small fontslabo" style="background-color:#2ecc71; width:100%;" >Proceed Booking</button-->
 
 		</div>
 	
@@ -311,7 +316,7 @@ $_SESSION['childrens'] = $_POST["totalchildrens"];
 
 	</div>
 
-</div>
+</div><br>
 
  <!-- ======= Footer ======= -->
  <footer id="footer">
@@ -319,12 +324,9 @@ $_SESSION['childrens'] = $_POST["totalchildrens"];
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-3 col-md-6 footer-info">
-            <h3>BizPage</h3>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-          </div>
+          
 
-          <div class="col-lg-3 col-md-6 footer-links">
+          <div class="col-lg-3 col-md-3 footer-links">
             <h4>Useful Links</h4>
             <ul>
               <li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>
@@ -335,7 +337,7 @@ $_SESSION['childrens'] = $_POST["totalchildrens"];
             </ul>
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-contact">
+          <div class="col-lg-3 col-md-3 footer-contact">
             <h4>Contact Us</h4>
             <p>
               A108 Adam Street <br>
@@ -355,24 +357,19 @@ $_SESSION['childrens'] = $_POST["totalchildrens"];
 
           </div>
 
-          <div class="col-lg-3 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-
-        </div>
-      </div>
-    </div>
+          
 
     <div class="container">
       <div class="copyright">
         &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
       </div>
       <div class="credits">
-       
+        <!--
+        All the links in the footer should remain intact.
+        You can delete the links only if you purchased the pro version.
+        Licensing information: https://bootstrapmade.com/license/
+        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
+      -->
         Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
