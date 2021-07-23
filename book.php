581eb0d48d88826d036b645ff9e2838f93994f0a
sparsh tiwari <sparsh.tiwariy@gmail.com>
2021-07-23 19:12:42 +0530

    no

--- book.php
+++ book.php
@@ -1,23 +1,18 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
-  <title>BizPage Bootstrap Template</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-
-  <meta name="zulkarnain" content="gambohnetwork.com.my">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Reservation</title>
+<meta name="reservation hotel for malaysia" >
+<meta name="zulkarnain" content="gambohnetwork.com.my">
 <meta name="copyright" content="Hotel Malaysia, inc. Copyright (c) 2014">
-
-  <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+<link rel="stylesheet" href="scss/normalize.css">
+<link rel="stylesheet" href="scss/foundation.css">
+<link rel="stylesheet" href="scss/style.css">
+<link rel="stylesheet" href="scss/datepicker.css">
+<link href="scss/datepicker.css" rel="stylesheet" type="text/css"/> 
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -28,275 +23,140 @@
   <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
   <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <meta name="reservation hotel for malaysia" >
-
-<link rel="stylesheet" href="scss/normalize.css">
-<link rel="stylesheet" href="scss/foundation.css">
-
-  <!-- Template Main CSS File -->
-  <link href="scss/style.css" rel="stylesheet">
 
 
-<link href="scss/datepicker.css" rel="stylesheet" type="text/css"/>  
 <link href='http://fonts.googleapis.com/css?family=Slabo+13px' rel='stylesheet' type='text/css'>
 <!--link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/-->
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<script>
+  $(document).ready(function() {
+		$("#checkout").datepicker();
+		$("#checkin").datepicker({
+		minDate : new Date(),
+		onSelect: function (dateText, inst) {
+        var date = $.datepicker.parseDate($.datepicker._defaults.dateFormat, dateText);
+        $("#checkout").datepicker("option", "minDate", date);
+		}
+		});
+  });
+</script>
+<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+<meta class="foundation-data-attribute-namespace">
+<meta class="foundation-mq-xxlarge">
+<meta class="foundation-mq-xlarge">
+<meta class="foundation-mq-large">
+<meta class="foundation-mq-medium">
+<meta class="foundation-mq-small"><style></style>
+<meta class="foundation-mq-topbar"></head>
+<body class="fontbody" style="background-image : url(img/background.jpg); position: center fixed; background-size: cover;">
 
-  <!-- =======================================================
-  * Template Name: BizPage - v3.2.1
-  * Template URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-</head>
 
-<body>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top header-transparent">
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top header-transparent">
     <div class="container-fluid">
 
-      <div class="row">
-        <div class="col-xl-12 d-flex align-items-center">
-          <h1 class="logo mr-auto"><a href="index.html">BizPage</a></h1>
+      <div class="row justify-content-center">
+        <div class="col-xl-11 d-flex align-items-center">
+          <h1 class="logo mr-auto"><a href="index.html">SR COTTAGE</a></h1>
           <!-- Uncomment below if you prefer to use an image logo -->
           <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
           <nav class="nav-menu d-none d-lg-block">
             <ul>
               <li class="active"><a href="index.html">Home</a></li>
-              <li><a href="#about">About Us</a></li>
+              <li><a href="#facility">Facilities</a></li>
               <li><a href="#services">Services</a></li>
-              <li><a href="#portfolio">Portfolio</a></li>
+              <li><a href="#gallery">Gallery</a></li>
+              <li><a href="#events">Events</a></li>
               <li><a href="#team">Team</a></li>
               <li><a href="#contact">Contact Us</a></li>
 
             </ul>
-          </nav>
-          <!-- .nav-menu -->
+          </nav><!-- .nav-menu -->
         </div>
       </div>
 
     </div>
-  </header>
-  <!-- End Header -->
-
-  <!-- ======= Intro Section ======= -->
-  <section id="intro">
-    <div class="intro-container">
-      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
-
-        <ol class="carousel-indicators"></ol>
-
-        <div class="carousel-inner" role="listbox">
-
-          <div class="carousel-item active" style="background-image: url(assets/img/intro-carousel/1.jpg)">
-            <div class="carousel-container">
-              <div class="container">
-                <h2 class="animate__animated animate__fadeInDown">We are professional</h2>
-                <p class="animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <a href="#featured-services" class="btn-get-started scrollto animate__animated animate__fadeInUp">Get Started</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item" style="background-image: url(assets/img/intro-carousel/2.jpg)">
-            <div class="carousel-container">
-              <div class="container">
-                <h2 class="animate__animated animate__fadeInDown">At vero eos et accusamus</h2>
-                <p class="animate__animated animate__fadeInUp">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut.</p>
-                <a href="#featured-services" class="btn-get-started scrollto animate__animated animate__fadeInUp">Get Started</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item" style="background-image: url(assets/img/intro-carousel/3.jpg)">
-            <div class="carousel-container">
-              <div class="container">
-                <h2 class="animate__animated animate__fadeInDown">Temporibus autem quibusdam</h2>
-                <p class="animate__animated animate__fadeInUp">Beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt omnis iste natus error sit voluptatem accusantium.</p>
-                <a href="#featured-services" class="btn-get-started scrollto animate__animated animate__fadeInUp">Get Started</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item" style="background-image: url(assets/img/intro-carousel/4.jpg)">
-            <div class="carousel-container">
-              <div class="container">
-                <h2 class="animate__animated animate__fadeInDown">Nam libero tempore</h2>
-                <p class="animate__animated animate__fadeInUp">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum.</p>
-                <a href="#featured-services" class="btn-get-started scrollto animate__animated animate__fadeInUp">Get Started</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item" style="background-image: url(assets/img/intro-carousel/5.jpg)">
-            <div class="carousel-container">
-              <div class="container">
-                <h2 class="animate__animated animate__fadeInDown">Magnam aliquam quaerat</h2>
-                <p class="animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <a href="#featured-services" class="btn-get-started scrollto animate__animated animate__fadeInUp">Get Started</a>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
-        <a class="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-
-        <a class="carousel-control-next" href="#introCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon ion-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-
-      </div>
-    </div>
-  </section>
-  <!-- End Intro Section -->
-
-
-
-  <div id="booking" class="section">
-    <div class="section-center">
-        <div class="container">
-            <div class="row">
-                <div class="booking-form">
-                    <div class="form-header">
-                        <h1>Make your reservation</h1>
-                    </div>
-                    <form name="form" action="checkroom.php" method="POST" onSubmit="return validateForm(this);">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-								<!-- <label for="checkin"> Check In -->
-                                    <input name="checkin" id="checkin" class="form-control" type="date" required>
-								<!-- </label> -->
-                                    <span class="form-label">Check In</span>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-								<!-- <label for="checkout"> Check Out -->
-                                    <input name="checkout" id="checkout" class="form-control" type="date" required>
-								<!-- </label> -->
-                                    <span class="form-label">Check out</span>
-                                </div>
-                            </div>
-                        </div><br>
-                        <div class="row">
-                            <!-- <div class="col-md-4">
-                                <div class="form-group">
-                                    <select name="totaladults" id="totaladults" class="form-control" required>
-                                        <option value="" selected hidden>no of rooms</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                    </select>
-                                    <span class="select-arrow"></span>
-                                    <span class="form-label">Rooms</span>
-                                </div>
-                            </div> -->
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <select name="totaladults" id="totaladults" class="form-control" required>
-                                        <option value="" selected hidden>no of adults</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                    </select>
-                                    <span class="select-arrow"></span>
-                                    <span class="form-label">Adults</span>
-                                </div>
-                            </div><br>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <select name="totalchildrens" id="totalchildrens" class="form-control" required>
-                                        <option value="" selected hidden>no of children</option>
-                                        <option>0</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                    </select>
-                                    <span class="select-arrow"></span>
-                                    <span class="form-label">Children</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-btn">
-                            <button name="submit" href="#"      class="submit-btn">Book Now</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> 
-
-
-
-<!-- ======= Footer ======= -->
-<footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          
-
-          <div class="col-lg-6 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Home</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">About us</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Services</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="ion-ios-arrow-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-6 col-md-6 footer-contact">
-            <h4>Contact Us</h4>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-
-            <div class="social-links">
-              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-            </div>
-
-          </div>
-
-          
-
-    <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
-      </div>
-      <div class="credits">
-        <!--
-        All the links in the footer should remain intact.
-        You can delete the links only if you purchased the pro version.
-        Licensing information: https://bootstrapmade.com/license/
-        Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
-      -->
-        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
-  <!-- Uncomment below i you want to use a preloader -->
-  <!-- <div id="preloader"></div> -->
+  </header><!-- End Header -->
+  <div class="row foo" style="margin:30px auto 30px auto;"><br><br>
+  <div class="row">
+	<div class="large-6 columns blackblur fontcolor" style="padding-top:10px;">
+	
+	<div class="large-12 columns " >
+	<p><b>Check Date</b></p><hr class="line">
+			<form name="form" action="checkroom.php" method="post" onSubmit="return validateForm(this);">
+			<div class="row">
+				
+					<div class="large-6 columns" style="max-width:100%;">
+						<label class="fontcolor" for="checkin">Check In
+							<input name="checkin" id="checkin" style="width:100%;"/>
+						</label>
+					</div>
+					
+					<div class="large-6 columns" style="max-width:100%;">
+						<label class="fontcolor" for="checkout">Check Out
+							<input name="checkout" id="checkout" style="width:100%;"/>
+						</label>
+					
+					
+					</div>
+			</div>
+					
+			<div class="row">
+				
+					<div class="large-6 columns">
+						<label class="fontcolor">Adults
+							
+								<select  name="totaladults" id="totaladults" style="width:100%;">
+								<option value="0">0</option>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+								<option value="7">7</option>
+								<option value="8">8</option>
+								<option value="9">9</option>
+								<option value="10">10</option>
+								</select>
+							
+						</label>
+					</div>
+					
+					<div class="large-6 columns"  style="max-width:100%;">
+						<label class="fontcolor">Children
+							<select  name="totalchildrens" id="totalchildrens" style="width:100%; color:black;">
+							<option value="0">0</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+							<option value="10">10</option>
+							</select>
+						</label>
+					</div>
+					
+				
+			</div>
+			
+			  <div class="row">
+				<div class="large-12 columns" >
+					<button name="submit" href="#" class="button small fontslabo" style="background-color:#2ecc71; width:100%;" >Check Availability</button>
+				</div>
+			  </div>
+			</form>
+	</div>
+	</div>
+</div>
+</div>
 
 <script>
 	function validateForm(form) {
@@ -338,25 +198,6 @@
   ga('create', 'UA-57205452-1', 'auto');
   ga('send', 'pageview');
 
-
 </script>
-
-
-
-<!-- Vendor JS Files -->
-<script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
-  <script src="assets/vendor/counterup/counterup.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-  </body>
-  </html>
+</body>
+</html>
